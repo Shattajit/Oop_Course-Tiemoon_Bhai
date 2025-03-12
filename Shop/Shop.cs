e19b028d049c0cf62e39c2005a0a9748e0fb2aed
Shattajit <jeet.shatta@gmail.com>
2025-03-12 20:53:00 +0600

    Shop design by C#

--- Shop/Shop.cs
+++ Shop/Shop.cs
@@ -10,9 +10,17 @@ class Shop
     public void ShowProducts()
     {
         Console.WriteLine("\nAvailable Products:");
-        foreach (var product in Products)
+        foreach(Product product in Products)
         {
             product.DisplayProductInfo();
+            /*
+                though product class is a abstarct class so we can't create an object of it but
+                List<Product> is storing grocery and accesories objects from caller side
+                ex:  shop.AddProduct(new Grocery(1, "Milk", 1.5, "2025-03-01"));
+                so it's a dynamic method dispatch... it's using reference of product class and storing object
+                of the subclass grocery and aceesories.. so when we call product.DisplayProductInfo() it
+                will run the overridden method in the sub class... cool!!
+            */
         }
     }
 
--- Shop/obj/Debug/net8.0/Shop.AssemblyInfo.cs
+++ Shop/obj/Debug/net8.0/Shop.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Shop")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+eae516e5dc5ac85e3da745098c9b6b17fef5ba15")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+888b19ca5c77ebf33a422bec453eedaf579027c5")]
 [assembly: System.Reflection.AssemblyProductAttribute("Shop")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Shop")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
